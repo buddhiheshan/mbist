0f83455d58ece9c8e9de0ff3379068608a87de3b
Buddhi Perera <ghp7482@nyu.edu>
2025-04-25 11:29:16 -0400

    Updated controller testbench

--- tests/tb_controller.sv
+++ tests/tb_controller.sv
@@ -1,17 +1,17 @@
 module tb_controller();
 
-    import controller_fsm_pkg::*;
+    typedef enum logic { RESET, TEST } state_t;
     
     logic start, rst, clk, cout, NbarT, ld;
 
     controller dut(.*);
 
-    task check_output(state_t expected_state, logic expected_ld, expected_NbarT);
-        if (dut.state !== expected_state || ld !== expected_ld || NbarT !== expected_NbarT) begin
-            $display("FAIL\tTime=%0t\t Expected: State:%s ld=%b NbarT=%b | Got: State:%s ld=%b NbarT=%b", $time, expected_state.name(), expected_ld, expected_NbarT, dut.state.name(), ld, NbarT);
+    task check_output(logic expected_ld, expected_NbarT);
+        if (ld !== expected_ld || NbarT !== expected_NbarT) begin
+            $display("FAIL\tTime=%0t\t Expected:  ld=%b NbarT=%b | Got:  ld=%b NbarT=%b", $time, expected_ld, expected_NbarT, ld, NbarT);
         end
         else begin
-            $display("PASS\tTime=%0t\t Expected: State:%s ld=%b NbarT=%b | Got: State:%s ld=%b NbarT=%b", $time, expected_state.name(), expected_ld, expected_NbarT, dut.state.name(), ld, NbarT);
+            $display("PASS\tTime=%0t\t Expected:  ld=%b NbarT=%b | Got:  ld=%b NbarT=%b", $time, expected_ld, expected_NbarT, ld, NbarT);
         end
     endtask
 
@@ -25,32 +25,32 @@ module tb_controller();
         cout = 0;
 
         #1 rst = 1;
-        #5 check_output(RESET, 1'b1, 1'b0);
+        #5 check_output(1'b1, 1'b0);
         #1 rst = 0;
 
         cout = 1;
-        #9 check_output(RESET, 1'b1, 1'b0);
+        #9 check_output(1'b1, 1'b0);
         #1 cout = 0;
 
         start = 1;
-        #9 check_output(TEST, 1'b0, 1'b1);
+        #9 check_output(1'b0, 1'b1);
         #1 start = 0;
 
         start = 1;
-        #9 check_output(TEST, 1'b0, 1'b1);
+        #9 check_output(1'b0, 1'b1);
         #1 start = 0;
 
         cout = 1;
-        #9 check_output(RESET, 1'b1, 1'b0);
+        #9 check_output(1'b1, 1'b0);
         #1 cout = 0;
 
         start = 1;
-        #9 check_output(TEST, 1'b0, 1'b1);
+        #9 check_output(1'b0, 1'b1);
         #1 start = 0;
 
         #1 rst = 1;
-        check_output(TEST, 1'b0, 1'b1);
-        #9 check_output(RESET, 1'b1, 1'b0);
+        check_output(1'b0, 1'b1);
+        #9 check_output(1'b1, 1'b0);
         #1 rst = 0;
         #10;
         $finish;
