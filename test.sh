cd98254a9371675a78c105a72a629f159f524cc5
Buddhi Perera <ghp7482@nyu.edu>
2025-04-25 11:39:09 -0400

    Updated test script

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
 DUT=$1.sv
 TB=tests/tb_$1.sv
 
-vcs -full64 -kdb -sverilog $DUT $TB -lca -debug_access+all
+if [ $1 = "bist" ]; then
+    vcs -full64 -kdb -sverilog sram.sv multiplexer.sv controller.sv comparator.sv decoder.sv counter.sv $DUT $TB -lca -debug_access+all
+else
+    vcs -full64 -kdb -sverilog $DUT $TB -lca -debug_access+all
+fi
+
 ./simv
