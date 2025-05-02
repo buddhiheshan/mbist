a3853521bea4e516fba2edca2013a8f61dcc2de3
Buddhi Perera <ghp7482@nyu.edu>
2025-05-02 15:50:17 -0400

    Updated bist design and testbench

--- bist.sv
+++ bist.sv
@@ -16,18 +16,17 @@ logic [7:0] data_t;
 logic [9:0] q;
 
 controller controller(.start(start), .rst(rst), .clk(clk), .cout(cout), .NbarT(NbarT), .ld(ld));
-counter #(.LENGTH(10)) counter(.d_in(), .clk(clk), .ld(ld), .u_d(), .cen(), .q(q), .cout(cout));
+counter #(.LENGTH(10)) counter(.d_in(0), .clk(clk), .ld(ld), .u_d(1), .cen(1), .q(q), .cout(cout));
 decoder decoder(.q(q[9:7]), .data_t(data_t));
-comparator comparator(.data_t(data_t), .ramout(dataout), .gt(), .eq(), .lt());
+comparator comparator(.data_t(data_t), .ramout(dataout), .gt(), .eq(eq), .lt());
 
 multiplexer #(.WIDTH(SIZE)) mux_a(.normal_in(address), .bist_in(q[5:0]), .NbarT(NbarT), .out(ramaddr_out));
 multiplexer #(.WIDTH(LENGTH)) mux_d(.normal_in(datain), .bist_in(data_t), .NbarT(NbarT), .out(ramin_out));
 multiplexer #(.WIDTH(1)) mux_rwbar(.normal_in(rwbarin), .bist_in(q[6]), .NbarT(NbarT), .out(rwbar_out));
 multiplexer #(.WIDTH(1)) mux_cs(.normal_in(csin), .bist_in(1'b1), .NbarT(NbarT), .out(cs_out));
 
-sram memeory(.ramaddr(ramaddr_out), .ramin(ramin_out), .rwbar(rwbar_out), .clk(clk), .cs(cs_out), .ramout(dataout));
+sram memory(.ramaddr(ramaddr_out), .ramin(ramin_out), .rwbar(rwbar_out), .clk(clk), .cs(cs_out), .ramout(dataout));
 
-// fail with BIST read operation and eq = 0
-assign fail = opr ? (NbarT && q[6] && ~eq) : (NbarT && rwbarin && ~eq);
+assign fail = NbarT && rwbar_out && ~eq && opr;
 
 endmodule
--- tests/tb_bist.sv
+++ tests/tb_bist.sv
@@ -1,3 +1,87 @@
 module tb_bist ();
-    
+    `define SIZE 6
+    `define LENGTH 8
+
+    logic start, rst, clk, csin, rwbarin, opr;
+    logic [`SIZE-1:0] address;
+    logic [`LENGTH-1:0] datain;
+    logic [`LENGTH-1:0] dataout;
+    logic fail;
+
+    bist #(.SIZE(`SIZE), .LENGTH(`LENGTH)) bist (.*);
+
+    always #5 clk = ~clk;
+
+    task check_output(logic expected_fail, logic [`LENGTH-1:0] expected_dataout);
+        if (fail !== expected_fail || dataout !== expected_dataout) begin
+            $display("FAIL\tTime=%0t\t Expected: fail=%b dataout=%b | Got: fail=%b dataout=%b", $time, expected_fail, expected_dataout, fail, dataout);
+        end
+        else begin
+            $display("PASS\tTime=%0t\t Expected: fail=%b dataout=%b | Got: fail=%b dataout=%b", $time, expected_fail, expected_dataout, fail, dataout);
+        end
+    endtask
+
+    initial begin
+        clk = 0;
+        rst = 1;
+        start = 0;
+        opr = 1;
+
+        
+        rwbarin = 0;
+        csin = 0;
+        address = 0;
+        datain = 10;
+
+        @(posedge clk) check_output(0, 0);
+
+        #1;
+        rst = 0;
+        rwbarin = 0;
+        csin = 1;
+        check_output(0, 0);
+
+        @(posedge clk) check_output(0, 0);
+
+        #1;
+        rwbarin = 1;
+
+        #1;
+        check_output(0, 10);
+
+        #1;
+        start = 1;
+        @(posedge clk);
+
+        #1;
+        check_output(0, 0);
+
+        @(posedge clk);
+        @(posedge clk);
+        @(posedge clk);
+
+        #1;
+        rst = 1;
+        @(posedge clk);
+
+        #1;
+        rwbarin = 1;
+        #1;
+        check_output(0, 8'b10101010);
+
+
+
+
+        // #6;
+        // rwbarin = 1;
+
+        // #1;
+        // check_output(0, 10);
+
+
+
+        #50;
+        $finish;
+    end
+
 endmodule
